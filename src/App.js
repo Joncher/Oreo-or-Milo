273eb367a540e53081714970d63ad460b0acb4a0
John Oncher <you@example.comjohnonhcer@gmail.com>
2021-11-13 15:40:46 -0500

    created React hooks and switch cases

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
+import React, {useState} from 'react'
 import './App.css';
 
 const App = () => {
+
+  
+    const [progress, setProgress] = useState('getUpload')
+    const [url, setImageURL] = useState(undefined)
+    const [errorMessage, setErrorMessage] = useState('')
+    
+const content = () => {
+    // eslint-disable-next-line default-case
+    switch(progress){
+      case 'getUpload' :
+        return <div>Please upload an image</div>
+      case 'uploading':
+        return <h2>Uploading...</h2>
+      case 'uploaded':
+        return <img source= {url} alt='uploaded'/>
+      case 'uploadError':
+        return (
+          <>
+            <div>Error message = {errorMessage}</div>
+            <div>Please upload an image</div>
+          </>
+        )
+    }
+  }
+
+
   return(
-    <div>
+    <div className = "App">
        <h1>Upload an Image of Milo and/or Oreo</h1>
+       {content()}
     </div>
   )
 }
