01363c3f01a69f7c58d8fda14055ad41f8b824b4
John Oncher <you@example.comjohnonhcer@gmail.com>
2021-11-17 06:40:56 -0500

    ImageUploader created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React, {useState} from 'react'
+import ImageUploader from 'react-images-upload'
 import './App.css';
+
+
+const UploadComponent = props =>{
+  <form>
+    <label>
+      File Upload URL:
+      <input id='urlInput' type='text' onChange={props.onUrlChange} value={props.url}></input>
+    </label>
+    <ImageUploader
+      key = "image-uploader"
+      withIcon = {true}
+      singleImage = {true}
+      withPreview = {true}
+      label = "Maximum size file: 5MB"
+      buttonText = "Choose File Image"
+      onChange = {props.onImage}
+      imgExtension = {['.jpg', '.png', '.jpng']}
+      maxFileSize= {5242880}
+    ></ImageUploader>
+  </form>
+}
 
 const App = () => {
 
@@ -7,12 +29,12 @@ const App = () => {
     const [progress, setProgress] = useState('getUpload')
     const [url, setImageURL] = useState(undefined)
     const [errorMessage, setErrorMessage] = useState('')
-    
+
 const content = () => {
     // eslint-disable-next-line default-case
     switch(progress){
       case 'getUpload' :
-        return <div>Please upload an image</div>
+        return <UploadComponent onUrlChange={onUrlChange} onImage={onImage} url={url}/>
       case 'uploading':
         return <h2>Uploading...</h2>
       case 'uploaded':
